a4eeb13ee13294a0321a49b9b1f29e24566000f2
Logan T <178672247+LD2343@users.noreply.github.com>
2026-03-22 16:00:46 -0500

    new repo

---
+++ 00-auth.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0" # Use latest version if possible
+    }
+  }
+
+  backend "s3" {
+    bucket  = "jenkins-ltd-class7"                 # Name of the S3 bucket
+    key     = "jenkins-test-032226.tfstate"        # The name of the state file in the bucket
+    region  = "us-east-1"                          # Use a variable for the region
+    encrypt = true                                 # Enable server-side encryption (optional but recommended)
+  } 
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
